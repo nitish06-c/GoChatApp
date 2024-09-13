c70125d1bb3616a9c01ba178cfb6be48a7191e81
Nitish Madabusi <nit.madabusi@gmail.com>
2024-09-13 15:42:15 -0400

    update: added comments

--- gochatapp/main.go
+++ gochatapp/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	go handlers.HandleMessages()
 
-	log.Println("Server started on :8080")
+	log.Println("Server started on :8080") // started server
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
